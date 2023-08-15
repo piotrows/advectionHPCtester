70793035dd27d208962a2bb0d1e082758895fc0f
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2023-08-15 16:27:47 +0200

    Working example of dynamic loading of sp and dp routines on Mac

--- advection/src_drivers/advec_interface_sp.f90
+++ advection/src_drivers/advec_interface_sp.f90
@@ -1,21 +1,22 @@
 MODULE advec_interface_sp
+   USE, INTRINSIC :: iso_c_binding
    IMPLICIT NONE
    CONTAINS
-   SUBROUTINE allocate_interface_sp(linitmpi, nprocx, nprocy, nprocz)
+   SUBROUTINE allocate_interface_sp(linitmpi, nprocx, nprocy, nprocz)  bind(c, name='allocate_interface_sp')
    USE advec_initialize, ONLY : allocate_and_initialize 
    LOGICAL, INTENT(IN) :: linitmpi
    INTEGER, INTENT(IN) :: nprocx, nprocy, nprocz
    CALL allocate_and_initialize(linitmpi, nprocx, nprocy, nprocz) 
    END SUBROUTINE allocate_interface_sp
    
-   SUBROUTINE deallocate_interface_sp(itime_counter)
+   SUBROUTINE deallocate_interface_sp(itime_counter) bind(c, name='deallocate_interface_sp')
    USE advec_initialize, ONLY : deallocate_and_finalize 
    INTEGER, INTENT(IN) :: itime_counter 
    CALL deallocate_and_finalize(itime_counter) 
    END SUBROUTINE deallocate_interface_sp
 
    SUBROUTINE advec_dwarf_interface_sp(legacyoptmode,t_adv,   &
-                           lupdatemulti, lvertsplit, ipoles,itime_counter)
+                           lupdatemulti, lvertsplit, ipoles,itime_counter) bind(c, name='advec_dwarf_interface_sp')
    USE advec_driver, ONLY: advec_dwarf
    USE scratch_datafields, ONLY: xtracer,bcx,bcy
    USE scratch_datafields, ONLY: rhr,rhoadv
