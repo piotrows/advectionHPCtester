cd57b1eb0cb1a85da4ae2f487583dea68ebc333d
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2023-08-25 09:06:22 +0000

    Correct datatypes in interface

--- advection/src_drivers/advec_init_interface_dpgpu.f90
+++ advection/src_drivers/advec_init_interface_dpgpu.f90
@@ -1,17 +1,26 @@
 MODULE advec_init_interface_dpgpu
    USE advec_initialize
+   USE :: iso_c_binding
    IMPLICIT NONE
 CONTAINS
    SUBROUTINE allocate_interface_dpgpu(linitmpi, nprocx, nprocy, nprocz)
    USE precisions
-   LOGICAL, INTENT(IN) :: linitmpi
-   INTEGER, INTENT(IN) :: nprocx, nprocy, nprocz 
-   CALL allocate_and_initialize(linitmpi, nprocx, nprocy, nprocz)
+   LOGICA(c_bool)L, INTENT(IN) :: linitmpi
+   LOGICAL :: lfinitmpi
+   INTEGER(c_int)R, INTENT(IN) :: nprocx, nprocy, nprocz 
+   INTEGER :: nfprocx, nfprocy, nfprocz 
+   lfinitmpi=linitmpi
+   nfprocx=nprocx
+   nfprocy=nprocy
+   nfprocz=nprocz
+   CALL allocate_and_initialize(lfinitmpi, nfprocx, nfprocy, nfprocz)
    END SUBROUTINE allocate_interface_dpgpu
    
 
    SUBROUTINE deallocate_interface_dpgpu(itime_counter)
-   INTEGER, INTENT(IN) :: itime_counter
-   CALL deallocate_and_finalize(itime_counter)
+   INTEGER(c_int), INTENT(IN) :: itime_counter
+   INTEGER :: iftime_counter
+   iftime_counter=itime_counter
+   CALL deallocate_and_finalize(iftime_counter)
    END SUBROUTINE deallocate_interface_dpgpu
 END MODULE advec_init_interface_dpgpu
